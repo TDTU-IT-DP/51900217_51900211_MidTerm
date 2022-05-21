9cdba2d47f2f309ffec2f8f873e1668986f5c37a
Nguyễn Trí Thanh <102459903+ThanhNG1311@users.noreply.github.com>
2022-05-22 03:38:16 +0700

    Commit class main thực thi chương trình.

---
+++ Demo/Client.java
@@ -0,0 +1,16 @@
+public class Client {
+    
+    public static void main(String[] args) {
+        Car car = new Car_HondaCity();
+        CarDecorator autoLock = new Deco_AutomaticLock(car);
+        CarDecorator dashCam = new Deco_DashCam(car);
+        CarDecorator rearViewCam = new Deco_RearViewCam(car);
+        if (car.BrandName().equals(null)) {
+            System.out.println("This brand is not supported.");
+        } else {
+            float total = car.Price() + dashCam.Price() + rearViewCam.Price() + autoLock.Price();
+            System.out.println("Total Price of " + car.BrandName() + " " + car.Name() + " with accessories: " + total + "$");
+        }
+    }
+    
+}
